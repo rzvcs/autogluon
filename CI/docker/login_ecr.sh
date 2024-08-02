702905ea5b5dc52815f23394a68eb6739573c1f9
Razvan Ionescu <razvan.ionescu@radcom.com>
2024-08-02 15:16:53 +0300

    chore(setup): remove upstream CI

--- CI/docker/login_ecr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 369469875935.dkr.ecr.us-east-1.amazonaws.com
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-east-1.amazonaws.com
